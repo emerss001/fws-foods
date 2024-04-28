d61fae7a72ec8f78d64afda5e32ebc27efe9f9aa
emerss001 <emersonnevess80@gmail.com>
2024-04-28 19:22:29 -0300

    chore: adicionando husky e lint setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+const name = "";
+console.log(name);
+
 const Home = () => {
   return (
-    <div className="px-2 bg-slate-900">
+    <div className="bg-slate-900 px-2">
       <h1>Helo Word!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
