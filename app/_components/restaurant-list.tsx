fd0b103c5198d4bbd07eec91736c8ba2a3454fe2
emerss001 <emersonnevess80@gmail.com>
2024-05-01 16:06:20 -0300

    feat: adicionando componente de lista de restaurantes

---
+++ app/_components/restaurant-list.tsx
@@ -0,0 +1,16 @@
+import { db } from "../_lib/prisma";
+import RestaurantItem from "./restaurant-item";
+
+const RestaurantList = async () => {
+  // TODO: pegar restaurantes com maior número de pedidios
+  const restaurants = await db.restaurant.findMany({ take: 10 });
+  return (
+    <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+      {restaurants.map((restaurant) => (
+        <RestaurantItem key={restaurant.name} restaurant={restaurant} />
+      ))}
+    </div>
+  );
+};
+
+export default RestaurantList;
