f380b9f3baea8b8706ed66dde6e49e34b956d0ca
emerss001 <emersonnevess80@gmail.com>
2024-05-07 00:11:19 -0300

    feat: adicionando página do restuarante

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -1,20 +1,15 @@
 "use client";
 
+import DevileryInfo from "@/app/_components/delivery-info";
 import DiscountBadge from "@/app/_components/discount-badge";
 import ProductList from "@/app/_components/product-list";
 import { Button } from "@/app/_components/ui/button";
-import { Card } from "@/app/_components/ui/card";
 import {
   calculateProductTotalPrice,
   formatCurrency,
 } from "@/app/_helpers/price";
 import { Prisma } from "@prisma/client";
-import {
-  BikeIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  TimerIcon,
-} from "lucide-react";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -104,35 +99,7 @@ const ProductDetails = ({
       </div>
 
       {/* DADOS DA ENTREGA */}
-      <Card className="mt-6 flex justify-around py-3">
-        {/* CUSTO */}
-        <div className="flex flex-col items-center">
-          <div className="flex items-center gap-1 text-muted-foreground">
-            <span className="text-xs">Entrega</span>
-            <BikeIcon size={14} />
-          </div>
-
-          {Number(product.restaurant.deliveryFee) > 0 ? (
-            <p className="text-xs font-semibold">
-              {formatCurrency(Number(product.restaurant.deliveryFee))}
-            </p>
-          ) : (
-            <p className="text-sm font-semibold">Grátis</p>
-          )}
-        </div>
-
-        {/* TEMPO */}
-        <div className="flex flex-col items-center">
-          <div className="flex items-center gap-1 text-muted-foreground">
-            <span className="text-xs">Entrega</span>
-            <TimerIcon size={14} />
-          </div>
-
-          <p className="text-xs font-semibold">
-            {product.restaurant.deliveryTimeMinutes} min
-          </p>
-        </div>
-      </Card>
+      <DevileryInfo restaurant={product.restaurant} />
 
       {/* DESCRIÇÃO */}
       <div className="mt-6 space-y-3">
