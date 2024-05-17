5946e91b3639d3124d8e7175ceef71deebf15725
emerss001 <emersonnevess80@gmail.com>
2024-05-17 10:02:59 -0300

    feat: garantir que usuário possa refazer pedido

--- app/my-orders/_components/order-item.tsx
+++ app/my-orders/_components/order-item.tsx
@@ -4,11 +4,14 @@ import { Avatar } from "@/app/_components/ui/avatar";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Separator } from "@/app/_components/ui/separator";
+import { CartContext } from "@/app/_context/cart";
 import { formatCurrency } from "@/app/_helpers/price";
 import { OrderStatus, Prisma } from "@prisma/client";
 import { ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
 
 interface OrderItemProps {
   order: Prisma.OrderGetPayload<{
@@ -39,6 +42,23 @@ const getOrderStatusLabel = (status: OrderStatus) => {
 };
 
 const OrderItem = ({ order }: OrderItemProps) => {
+  const { addProductToCart } = useContext(CartContext);
+  const router = useRouter();
+
+  const hendleRedoOrderClick = () => {
+    for (const orderProduct of order.products) {
+      addProductToCart({
+        product: {
+          ...orderProduct.product,
+          restaurant: order.restaurants,
+          quantity: orderProduct.quantity,
+        },
+      });
+    }
+
+    router.push(`/restaurants/${order.restaurantId}`);
+  };
+
   return (
     <Card>
       <CardContent className="p-5">
@@ -107,6 +127,7 @@ const OrderItem = ({ order }: OrderItemProps) => {
             size="sm"
             className="text-xs text-primary"
             disabled={order.status !== "COMPLETED"}
+            onClick={hendleRedoOrderClick}
           >
             Refazer pedido
           </Button>
